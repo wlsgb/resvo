b7926988522575d715f6b1c59fe625788686a81f
wlsgb94 <wlsgb94@naver.com>
2022-04-26 20:05:45 +0900

    first commit

---
+++ src/main/java/com/boot/resvo/ResvoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.resvo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResvoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/resvo/ResvoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.resvo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResvoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
