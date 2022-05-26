be14160b438844adf31c3e6878a3318377bf8778
wlsgb94 <wlsgb94@naver.com>
2022-05-26 22:46:58 +0900

    [기능추가] 회원 Entity 추가

--- src/main/java/com/boot/resvo/member/model/MemberEntity.java
+++ src/main/java/com/boot/resvo/member/model/MemberEntity.java
@@ -1,14 +1,24 @@
 package com.boot.resvo.member.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.persistence.Column;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -23,26 +33,32 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity(name = "member")
-public class MemberEntity {
+public class MemberEntity implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String memId;
+	private Long memId;
 
-	@Column(nullable = false)
+	@Column(length = 200, nullable = false, unique = true)
 	private String email;
 
-	@Column(nullable = false)
-	private String passward;
+	@Column(length = 100, nullable = false)
+	private String password;
 
+	@ElementCollection(fetch = FetchType.EAGER)
+	@Builder.Default
+	private List<String> roles = new ArrayList<>();
+
+	@Column(length = 100, unique = true)
 	private String nickNm;
 
-	@Column(nullable = false)
+	@Column(length = 100, nullable = false)
 	private String memNm;
 
+	@Column(length = 20, unique = true)
 	private String telNo;
 
-	@Column(nullable = false)
+	@Column(length = 20, nullable = false, unique = true)
 	private String cpNo;
 
 	@Temporal(TemporalType.TIMESTAMP)
@@ -51,4 +67,40 @@ public class MemberEntity {
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date updtDate;
 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+			.map(SimpleGrantedAuthority::new)
+			.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
